9e787c18986058257952f31233f6d4769a00ba52
Alex <alexcaste2002gs@gmail.com>
2023-09-29 09:53:45 +0200

    idk

---
+++ UD2/Proyect_UD2/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Runtime.getRuntime().exec("C:/Program Files/JetBrains/IntelliJ IDEA Community
+        // Edition 2023.2.1/bin/idea64.exe");
+        
+        // System.out.println("User: "+System.getProperty("user.home"));
+        // System.out.println("OS: "+System.getProperty("os.name"));
+
+        Runtime.getRuntime().exec("explorer http://google.com");
+        Runtime.getRuntime().exec();
+        
+    }
+}
