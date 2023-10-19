950af63292e27f91d39e9edb9c19be4853736e61
Alex <alexcaste2002gs@gmail.com>
2023-10-19 13:15:34 +0200

    new branch

--- UD2/Proyect_UD2/src/ejsdiapos/adivina/GuesserLauncher.java
+++ UD2/Proyect_UD2/src/ejsdiapos/adivina/GuesserLauncher.java
@@ -10,16 +10,16 @@ public class GuesserLauncher {
     static GuessManager gm;
 
     public static void main(String[] args) {
-        
-        double generatedNumber = ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
+        double topValue = 50;
+        double generatedNumber = ThreadLocalRandom.current().nextInt((int) topValue);
         gm = new GuessManager(generatedNumber);
-        System.out.println("Numero generado: "+generatedNumber);
+        System.out.println("Numero generado: " + generatedNumber);
 
-        final int ThreadAmount = 10;
-
+        final int threadAmount = 10;
         List<Thread> threadList = new ArrayList<>();
-        for (int i = 0; i < ThreadAmount; i++) {
-            Thread newThread =new Thread(new GuesserThread(gm));
+
+        for (int i = 0; i < threadAmount; i++) {
+            Thread newThread = new Thread(new GuesserThread(gm,topValue));
             threadList.add(newThread);
             newThread.start();
         }
@@ -31,59 +31,62 @@ public class GuesserLauncher {
                 throw new RuntimeException(e);
             }
         }
-
     }
 }
 
 class GuesserThread implements Runnable {
-    String name;
-    GuessManager gm;
-    //TODO: contar intentos
+    private final String name;
+    private final GuessManager gm;
+    private final long startTime = System.currentTimeMillis();
+    private static int threadCounter = 1;
+    private final double maxNumber;
 
-    static int threadCounter = 1;
+    private long tries;
 
-    public GuesserThread(GuessManager gm) {
+    public GuesserThread(GuessManager gm, double maxNumber) {
         this.gm = gm;
-        this.name = "[Thread "+(threadCounter++)+"]";
+        this.maxNumber = maxNumber;
+        this.name = "[Thread " + (threadCounter++) + "]";
+    }
+
+    private long currentRuntime (){
+        return (System.currentTimeMillis()-startTime);
     }
 
     @Override
     public void run() {
+        System.out.println(name + ": Busqueda iniciada");
+
         int result;
-        System.out.println(name+": Busqueda iniciada");
         do {
-            result = gm.checkGuess(ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE));
+            result = gm.checkGuess(ThreadLocalRandom.current().nextInt((int) maxNumber));
+            tries++;
         } while (result == 0);
 
-        if(result==1){
-            System.out.println(name+": He encontrado el numero");
+        if (result == 1) {
+            System.out.printf("%s: He encontrado el numero (%d intentos)%n", name,tries);
         } else {
-            System.out.println(name+": Numero encontrado por otro hilo");
+            System.out.printf("%s: Numero encontrado por otro hilo (%d intentos)%n", name,tries);
         }
+
     }
-
-
 }
 
 class GuessManager {
-    double value;
-    boolean found = false;
+    private final double value;
+    private boolean found = false;
 
-    //TODO: poner limite inferior
     public GuessManager(double maxValue) {
         this.value = maxValue;
     }
 
-
     /**
-     *
      * @param n Numero que se quiere comprobar
      * @return -1: Encontrado por otro
-     *          0: No acertado
-     *          1: Acertado
+     * 0: No acertado
+     * 1: Acertado
      */
     synchronized int checkGuess(double n) {
-
         if (found) {
             return -1;
         } else if (n == value) {
@@ -91,9 +94,5 @@ class GuessManager {
             return 1;
         }
         return 0;
-
     }
-
 }
-
-
